a47125ec8dc0c9c0e62159e05197a87b636f650b
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-22 01:50:26 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,7 +6,8 @@ attr_accessor :total, :discount, :price, :items
    @items = []
  end
  def add_item(item, price, quantity = 1)
-   @total = (@total.to_f * quantity.to_f) + price
+   new_item = @total.to_f * quantity.to_f
+   @total = new_item + price
 
  end
 end
