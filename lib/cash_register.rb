284b0376ffb62ff93a48a5d2756175348aa96992
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-22 01:45:40 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,9 @@
 class CashRegister
-  def initialize
-    @total = 0
-    self.discount(discount)
-  end
-  def discount(discount=0)
-    @discount = discount
-  end
+attr_accessor :total, :discount, :price, :items
+ def initialize(discount = 0)
+   @total = 0 
+   @discount = discount
+   @items = []
+ end
 
 end
