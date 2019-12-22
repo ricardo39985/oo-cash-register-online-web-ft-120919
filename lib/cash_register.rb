3fcd39bc650afdb6bc2e4ff6bfa13ba4bb0b7935
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-22 01:40:08 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,13 +2,13 @@ class CashRegister
   def initialize(discount=0)
     @total = 0
   end
-  # def discount(discount=0)
-  #   puts discount
-  #   if discount > 0
-  #     @discount = discount.to_f/100.0
-  #     @total-(@total*@discount)
-  #   else
-  #     @total
-  #   end
-  # end
+  def discount(discount=0)
+    puts discount
+    if discount > 0
+      @discount = discount.to_f/100.0
+      @total-(@total*@discount)
+    else
+      @total
+    end
+  end
 end
